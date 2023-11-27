aefaa6a7a2298f8d8c22b316aefbfaae2f8cfa55
Daniel <daniel.lam3987@gmail.com>
2023-11-26 20:35:38 -0500

    Second commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 import os
+import base64
 
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-print(client_id, client_secret)
-print(dog)
+def get_token():
+    
